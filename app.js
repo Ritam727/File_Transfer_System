84c07cf6e8863719c6b560c2b3fee89911ceaf2a
Ritam727 <B20127@students.iitmandi.ac.in>
2023-05-09 18:11:50 +0530

    Making the server

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
+const server = require("http").createServer(app);
 
-app.listen(5000, function () {
+const io = require("socket.io")(server);
+
+io.on("connection", function (socket) {
+    socket.on("sender-join", function (data) {
+        socket.join(data.uid);
+    });
+
+    socket.on("receiver-join", function (data) {
+        socket.join(data.uid);
+        socket.in(data.sender_uid).emit("init", data.uid);
+    });
+
+    socket.on("file-meta", function (data) {
+        socket.in(data.uid).emit("fs-meta", data.metadata);
+    });
+
+    socket.on("fs-start", function (data) {
+        socket.in(data.uid).emit("fs-share", {});
+    });
+
+    socket.on("file-raw", function (data) {
+        socket.in(data.uid).emit("fs-share", data.buffer);
+    })
+});
+
+server.listen(5000, function () {
     console.log("Listening at 5000");
 });
