d446e80333207945586e7461b35165f04202f07b
Sachin737 <mahawarsachin31@gmail.com>
2023-05-09 23:13:00 +0530

    Landing page design done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,46 @@
 import React from "react";
+import "../home.scss";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/createroom`;
+    navigate(path);
+  };
+
   return (
-    <div className="mainPage">
-      <div className="container">
-        <div className="mainData">
-          <img src={require("../images/logo.png")} alt="" />
-          <h1>Our Innotvative File Transfer System</h1>
-          <h1>Streamlines your Workflow</h1>
-          <h2>
-            Experience lightning-fast,secure and hassle free file transfers with
-            out cutting-edge platform
-          </h2>
-          <button>Get Started</button>
+    <>
+      <div className="mainPage">
+        <div className="container">
+          <div className="mainData">
+            <div className="Logo">
+              <img src={require("../images/logo.png")} alt="" />
+            </div>
+
+            <div className="content">
+              <h1 className="tagLine">
+                Our Innovative <br />  File Transfer System <br />
+                Streamlines your Workflow
+              </h1>
+              <h3>
+                Experience lightning-fast,secure and hassle free file
+                transfers with our cutting-edge platform
+              </h3>
+            </div>
+
+            <span>
+              <button onClick={routeChange}>Get Started</button>
+            </span>
+          </div>
+          <div className="bgImage">
+            <img src={require("../images/main-bg.png")} alt="" />
+          </div>
         </div>
-        <img src={require("../images/main-bg.png")} alt="" />
       </div>
-    </div>
+    </>
   );
 };
 
 export default Home;
+
